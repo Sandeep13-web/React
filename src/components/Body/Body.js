c98dd8016ad9853b3af0dfdaf3a51668eaf930eb
Sandeep13-web <sandeep.risal2000@gmail.com>
2021-05-25 14:28:53 +0545

    Before props and useState

---
+++ src/components/Body/Body.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Body() {
+    return (
+        <div class ='Body'>
+            <div class = "main_area">
+                <h3>Welcome To the Website!!</h3>
+            </div>
+        </div>
+    );
+}
---
+++ src/components/LeftNav/LeftNav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function RightNav() {
+    return (
+        // start here
+        <div class = "LeftNav">
+            <center>
+                <h4>Welcome😀</h4>
+            </center>
+            <a href ="#">Dashboard</a>
+            <a href ="#">Profile</a>
+            <a href ="#">Academics</a>
+            <a href ="#">Achievment</a>
+            <a href ="#">Contact</a>
+        </div>
+    )
+}
