b035cc296d1bc52e8c3ef67a30d61b081c205465
Sandeep13-web <sandeep.risal2000@gmail.com>
2021-05-25 16:32:45 +0545

    Props and useState

--- src/App.js
+++ src/App.js
@@ -6,12 +6,26 @@ import Body from './components/Body/Body';
 import './components/Body/body.css';
 import RightNav from './components/LeftNav/LeftNav';
 import './components/LeftNav/LeftNav.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [name , setName] = useState("Sandeep");
+  const [days , setDay] = useState("3")
+        const buttonClick=() =>{  
+          setName("John");
+          setDay("7")
+        }
   return (
     <div>
-        <Header />
-        <Body />
+        <Header click = {buttonClick}>
+          <div>
+            This is use of props
+          </div>
+
+        </Header>
+
+        <Body name={name} days = {days}  click ={buttonClick}/>
         <RightNav />
     </div>
     
--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-export default function Body() {
+export default function Body(props) {
     return (
+        
         <div class ='Body'>
             <div class = "main_area">
                 <h3>Welcome To the Website!!</h3>
+                <h2>{props.name} has been working on React for {props.days} days.</h2>
+                <button class = 'btn'onClick={props.click}>Submit</button>
             </div>
         </div>
     );
